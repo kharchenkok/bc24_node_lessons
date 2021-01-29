f4ad1a3d52879b49514d66e3552c22836879c6ba
kharchenkok <k.v.kharchenko@ukr.net>
2021-01-29 14:23:28 +0200

    express-basics added

---
+++ express_basics/view-engine-examples/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {join} from "path"
+const PORT = 3000;
+
+const server = express();
+// join(__dirname, 'views')
+// server.set("views", join("./views"));
+server.set("views", "./views");
+server.set("view engine", "ejs");
+
+server.get("/", (req, res) => {
+  res.render("home", { id: Math.round(Math.random() * 10) });
+});
+
+server.listen(PORT, () => {
+  console.log("Started listening on port", PORT);
+});
