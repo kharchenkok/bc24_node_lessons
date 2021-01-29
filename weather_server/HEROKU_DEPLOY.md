58f41a52031be0cb46aaf5ba20cabfa7bc204591
kharchenkok <k.v.kharchenko@ukr.net>
2021-01-29 21:13:58 +0200

    add heroku

---
+++ weather_server/HEROKU_DEPLOY.md
@@ -0,0 +1,8 @@
+Step to deploy server on heroku
+1. Install Heroku CLI
+2. Run heroku login
+3. Run heroku create
+4. Get PORT from process.env
+5. Write in package.json start script
+6. Add env variables to heroku dyno
+7. Push code to heroku repository (git push heroku master)
---
+++ weather_server/server.js
@@ -0,0 +1,120 @@
+import express from "express";
+import Joi from "joi";
+import axios from "axios";
+import dotenv from "dotenv";
+import path from "path";
+
+const { pathname } = new URL(import.meta.url);
+// console.log("pathname",pathname);
+const __dirname = path.dirname(pathname);
+// console.log("__dirname",__dirname);
+
+dotenv.config();
+// dotenv.config({path:path.join(__dirname,".env")});
+
+const server = express();
+
+server.get("/weather", validateGetWeather, async(req,res)=>{
+  const { ln, lat } = req.query;
+    const forecastResponse = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${ln}&appid=${process.env.OPEN_WEATHER_API_KEY}`
+    )
+    res.send(forecastResponse.data)
+})
+
+function validateGetWeather(req, res, next) {
+  const getWeatherSchema = Joi.object({
+    ln: Joi.number().min(-180).max(180).required(),
+    lat: Joi.number().min(-90).max(90).required(),
+  });
+
+  const validationResult = getWeatherSchema.validate(req.query);
+  if (validationResult.error) {
+    return res.status(400).send(validationResult.error);
+  }
+
+  next();
+}
+
+server.listen(process.env.PORT, () => {
+  console.log("Server started listening on port", process.env.PORT);
+});
+
+
+
+
+// import express from "express";
+// import Joi from "joi";
+// import axios from "axios";
+// import dotenv from "dotenv";
+// import path from "path";
+// import cors from "cors";
+
+// const { pathname } = new URL(import.meta.url);
+// const __dirname = path.dirname(pathname);
+
+// dotenv.config({ path: path.join(__dirname, ".env") });
+// const server = express();
+
+// // BETTER DO NOT USE THIS!!!!!!!
+// // server.use(cors());
+
+// server.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       const whitelist = process.env.ALLOWED_ORIGINS.split(",");
+//       if (whitelist.indexOf(origin) !== -1) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//   })
+// );
+// /*
+// server.use((req, res, next) => {
+//   res.set("Access-Control-Allow-Origin", "http://localhost:3000");
+//   next();
+// });
+// server.options((req, res) => {
+//   res.set(
+//     "Access-Control-Allow-Methods",
+//     req.get("Access-Control-Request-Method")
+//   );
+//   res.set(
+//     "Access-Control-Allow-Headers",
+//     req.get("Access-Control-Allow-Headers")
+//   );
+//   res.set("Access-Control-Allow-Credentials", "true");
+//   res.send();
+// });
+// */
+
+// server.use(express.json());
+
+// server.get("/weather", validateGetWeather, async (req, res) => {
+//   const { ln, lat } = req.query;
+//   const forecastResponse = await axios.get(
+//     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${ln}&appid=${process.env.OPEN_WEATHER_API_KEY}`
+//   );
+
+//   res.send(forecastResponse.data);
+// });
+
+// function validateGetWeather(req, res, next) {
+//   const getWeatherSchema = Joi.object({
+//     ln: Joi.number().min(-180).max(180).required(),
+//     lat: Joi.number().min(-90).max(90).required(),
+//   });
+
+//   const validationResult = getWeatherSchema.validate(req.query);
+//   if (validationResult.error) {
+//     return res.status(400).send(validationResult.error);
+//   }
+
+//   next();
+// }
+
+// server.listen(process.env.PORT, () => {
+//   console.log("Server started listening on port", process.env.PORT);
+// });
